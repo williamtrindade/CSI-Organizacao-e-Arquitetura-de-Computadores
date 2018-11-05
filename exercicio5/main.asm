cb084d4170bc30b596bfe964a2ed50759c34bd7f
William Trindade <williamtrindade777@gmail.com>
2018-11-05 14:51:13 -0200

    Create main.asm

---
+++ exercicio5/main.asm
@@ -0,0 +1,24 @@
+; int *a = {10, 20, 30, 40};
+; int b = 5;
+; int i;
+; int x = 0;
+; for (i = 0; i < 4; i++)
+; x+= a[i] * b;
+
+JMP start
+	.a: DB 1
+	    DB 2
+            DB 3
+            DB 4
+
+	.b: DB 5
+
+	.x: DB 0
+	
+	.i: DB 0
+
+start:
+	MOV A, .i
+	MOV B
+
+inifor
