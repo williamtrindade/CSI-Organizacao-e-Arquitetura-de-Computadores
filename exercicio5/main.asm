3a03c8a40656c9534052ddeadc78348b0d65300d
William Trindade <williamtrindade777@gmail.com>
2018-11-07 14:03:11 -0200

    Update main.asm

--- exercicio5/main.asm
+++ exercicio5/main.asm
@@ -5,28 +5,31 @@
 ; for (i = 0; i < 4; i++)
 ; x+= a[i] * b;
 
-JMP start
-	.a: DB 1
-	    DB 2
-            DB 3
-            DB 4
-
-	.b: DB 5
-	.x: DB 0
-	.i: DB 0
+	JMP start
+vet_a: DB 1
+    DB 2
+    DB 3
+    DB 4
+varb: DB 5
+varx: DB 0
+vari: DB 0
 
 start:
-	JMP for
-		for:	
-			MOV A, .a
-			MOV B, .x
-			MOV C, .i
-			
-			
-			INC A
-			CMP A, 4
-			JAE fimfor
+	MOV D, [vari]
+ini_for:CMP D, 4d    ; d de decimal
+	JAE fora_for
+	ADD D, vet_a
+	MOV D, [D]
+	MOV A, [varb]
+	MUL D
+	MOV [varx], A
+	MOV D, [vari]
+	INC D
+	MOV [vari], D
+	JMP ini_for
+
+fora_for:	
+	
 
 
-		fimfor:
-			hlt
+fimfor:
