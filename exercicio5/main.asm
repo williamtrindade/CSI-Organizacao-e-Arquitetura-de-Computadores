8d4dee16dca4f437d3cbcc3ff7c2e68e9e489cea
William Trindade <williamtrindade777@gmail.com>
2018-11-07 14:51:25 -0200

    Update main.asm

--- exercicio5/main.asm
+++ exercicio5/main.asm
@@ -6,30 +6,46 @@
 ; x+= a[i] * b;
 
 	JMP start
-vet_a: DB 1
-    DB 2
-    DB 3
-    DB 4
-varb: DB 5
-varx: DB 0
-vari: DB 0
+vet_a:	DB 1
+     	DB 2
+		DB 3
+      	DB 4
+varb: 	DB 5
+varx: 	DB 0
+vari: 	DB 0
+b_  :   DB 100
 
 start:
-	MOV D, [vari]
+		MOV D, [vari]
 ini_for:CMP D, 4d    ; d de decimal
-	JAE fora_for
-	ADD D, vet_a
-	MOV D, [D]
-	MOV A, [varb]
-	MUL D
-	MOV [varx], A
-	MOV D, [vari]
-	INC D
-	MOV [vari], D
-	JMP ini_for
-
+		JAE fora_for
+		ADD D, vet_a
+		MOV D, [D]
+		MOV A, [varb]
+		MUL D
+		MOV [varx], A
+		MOV D, [vari]
+		INC D
+		MOV [vari], D
+		JMP ini_for
 fora_for:	
-	
+		MOV C, hello ; ponteiro para variavel
+		MOV D, 232   ; outpoot
+		CALL printNum
+		HLT          ; stop execution
+printNum:
+     	PUSH A
+     	PUSH B 
+     	MOV B, [b_]
 
-
-fimfor:
+.do:
+		MOV A, [C] ; carrega num
+		DIV B  
+		MOV B,  A    
+		DIV 10     
+		MUL 10     
+		SUB B, A
+		ADD B, 48
+		MOV [D], B
+		INC D 
+		
